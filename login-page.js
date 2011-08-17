3f1213629179444f41727dd69cf79fb1f168ae06
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-08-17 21:11:05 +0200

    Simpler version
    No background page, no page action icon, use jQuery, ...

--- login-page.js
+++ login-page.js
@@ -1,20 +1,10 @@
-// TODO Disable autocomplete to make browser itself to remember inputs
+$('form input:lt(2)').each(function(i) {
 
-var form = document.getElementsByTagName('form')[0]
-var inputs = form.getElementsByTagName('input')
-var rememberedInputs = { user: inputs[0], nif: inputs[1] }
+    var storageId = 'rememberedInputs.' + i
 
-for (var name in rememberedInputs) {
-    (function() {
-        var _name = name
-        rememberedInputs[_name].addEventListener('change', function() {
-            console.log(_name)
-            localStorage.setItem('rememberedInputs.' + _name, this.value)
-        })
-    })()
-}
+    this.value = localStorage.getItem(storageId)
 
-for (var name in rememberedInputs) {
-    var value = localStorage.getItem('rememberedInputs.' + name)
-    rememberedInputs[name].value = value
-}
+    $(this).change(function() {
+        localStorage.setItem(storageId, this.value)
+    })
+})
--- public-page.js
+++ public-page.js
@@ -1,6 +1,8 @@
 var howToUrl = 'http://www.triodos.es/es/particulares/como-operar/bienvenido-oficina-internet/'
 var loginUrl = 'https://banking.triodos.es/isum/services/redirect_acceso.jsp?url=https://banking.triodos.es/isum/Main?ISUM_SCR=login&loginType=accesoSeguro&ISUM_Portal=93&acceso_idioma=es_ES'
-var as = document.getElementsByTagName('a')
-for (var i = 0; i < as.length; i++) {
-    if (as[i].href == howToUrl) as[i].href = loginUrl
-}
+$('a').each(function() {
+    if (this.href == howToUrl && this.target == '_blank') {
+        this.href = loginUrl
+        this.target = ''
+    }
+})
