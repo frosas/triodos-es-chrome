5c2adec8817c66b4f8765459f06d53285e5072ba
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-09-04 13:30:58 +0200

    "Cómo operar" link didn't match
    Now it opens in the same window

--- public-page.js
+++ public-page.js
@@ -1,8 +1,5 @@
 var howToUrl = 'http://www.triodos.es/es/particulares/como-operar/bienvenido-oficina-internet/'
 var loginUrl = 'https://banking.triodos.es/isum/services/redirect_acceso.jsp?url=https://banking.triodos.es/isum/Main?ISUM_SCR=login&loginType=accesoSeguro&ISUM_Portal=93&acceso_idioma=es_ES'
-$('a').each(function() {
-    if (this.href == howToUrl && this.target == '_blank') {
-        this.href = loginUrl
-        this.target = ''
-    }
+$('#globalNavWrapper a').each(function() {
+    if (this.href == howToUrl) this.href = loginUrl
 })
