79f8fce09309dc852afed0951ae611d15e2d0d4f
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-01-25 01:02:36 +0100

    Focus password field

--- login-page.js
+++ login-page.js
@@ -9,6 +9,9 @@ var setInputRemembered = function(input, id) {
 var inputs = document.querySelectorAll('form input')
 var USUARIO_INDEX = 0
 var NIF_INDEX = 1
+var PASSWORD_INDEX = 2
 
 setInputRemembered(inputs[USUARIO_INDEX], USUARIO_INDEX)
 setInputRemembered(inputs[NIF_INDEX], NIF_INDEX)
+
+inputs[PASSWORD_INDEX].focus()
